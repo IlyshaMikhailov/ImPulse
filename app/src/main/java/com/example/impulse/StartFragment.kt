47fe0cae482177f52af6653422d386f2efe3437a
Kirill <kirillefr45@gmail.com>
2023-07-04 17:26:20 +0300

    добавил start fragment

--- app/src/main/java/com/example/impulse/StartFragment.kt
+++ app/src/main/java/com/example/impulse/StartFragment.kt
@@ -1,6 +1,25 @@
 package com.example.impulse
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.impulse.databinding.StartFragmentBinding
 
-class StartFragment:Fragment(R.layout.start_fragment) {
+class StartFragment : Fragment(R.layout.start_fragment) {
+    private var binding: StartFragmentBinding? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = StartFragmentBinding.bind(view)
+        binding?.run {
+            button.setOnClickListener {
+                findNavController().navigate(R.id.action_startFragment_to_mainFragment)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
