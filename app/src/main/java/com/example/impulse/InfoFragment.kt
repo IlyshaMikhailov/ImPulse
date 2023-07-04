2815d5b75010cfa2ce968ebc63f7e2733655fc52
Adjoz <minnegaleev2005@mail.ru>
2023-07-04 17:22:28 +0300

    Add menu_bottom.xml

--- app/src/main/java/com/example/impulse/InfoFragment.kt
+++ app/src/main/java/com/example/impulse/InfoFragment.kt
@@ -1,6 +1,19 @@
 package com.example.impulse
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.impulse.databinding.InfoFragmentBinding
 
 class InfoFragment:Fragment(R.layout.info_fragment) {
+    private var binding: InfoFragmentBinding?=null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = InfoFragmentBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
--- app/src/main/java/com/example/impulse/SettingsFragment.kt
+++ app/src/main/java/com/example/impulse/SettingsFragment.kt
@@ -1,6 +1,19 @@
 package com.example.impulse
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.impulse.databinding.SettingsFragmentBinding
 
 class SettingsFragment : Fragment(R.layout.settings_fragment) {
+    private var binding: SettingsFragmentBinding?=null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = SettingsFragmentBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
