829d1cffe7a25e642a789f9e46d1acdb88544182
EgorCharntsev <charncev200447@gamil.com>
2023-07-06 12:50:08 +0300

    update settings

--- app/src/main/java/com/example/impulse/ProfileFragment.kt
+++ app/src/main/java/com/example/impulse/ProfileFragment.kt
@@ -13,24 +13,24 @@ class ProfileFragment : Fragment(R.layout.profile_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = bind(view)
+
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
         val name = sharedPref?.getString("Name", "")
         val age = sharedPref?.getString("Age", "")
         val height = sharedPref?.getString("Height", "")
         val weight = sharedPref?.getString("Weight", "")
         val dsWeight = sharedPref?.getString("DesiredWeight", "")
+
         binding!!.tvTextName.text = name
         binding!!.tvTextAge.text = age
         binding!!.tvTextHeight.text = height
         binding!!.tvTextWeight.text = weight
         binding!!.tvTextDsWeight.text = dsWeight
+
         binding?.run {
             btnSettings.setOnClickListener {
                 findNavController().navigate(R.id.action_profileFragment_to_settingsFragment)
             }
-
-
-
         }
     }
 
