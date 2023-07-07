99dfd90ab761727a6b6bec27603fc6498af4a65d
ILYSHAMIKHAILOV <mix.il2812@gmail.com>
2023-07-07 17:42:21 +0300

    exerciserepositore add

--- app/src/main/java/com/example/impulse/Exercise.kt
+++ app/src/main/java/com/example/impulse/Exercise.kt
@@ -1,7 +1,10 @@
 package com.example.impulse
 
-data class Exercise(
+data class Exercise (
+    val id: Int,
+    val bodyPart: String,
     val title: String,
-    val time: String,
-    val image: String
+    var isFavourites: Boolean,
+    val description: String,
+    val gifka: Int,
 )
