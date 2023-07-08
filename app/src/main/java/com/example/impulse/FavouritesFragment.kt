41d39596ee58c0b9b76a5b70efe94624645cc280
EgorCharntsev <charncev200447@gamil.com>
2023-07-08 14:39:47 +0300

    update Favorites Fragment

--- app/src/main/java/com/example/impulse/FavouritesFragment.kt
+++ app/src/main/java/com/example/impulse/FavouritesFragment.kt
@@ -4,14 +4,44 @@ package com.example.impulse
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.impulse.databinding.FavouritesFragmentBinding
 
 class FavouritesFragment : Fragment(R.layout.favourites_fragment) {
     private var binding: FavouritesFragmentBinding? = null
+    private var adapter: FavouritesAdapter? = null
+    private var listOfExercise = ArrayList<Exercise>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FavouritesFragmentBinding.bind(view)
+
+        for (exercise in ExerciseRepository.list) {
+            if (exercise.isFavourites) {
+                listOfExercise.add(exercise)
+            }
+        }
+
+        initAdapter()
+
     }
+
+    private fun initAdapter() {
+        adapter = listOfExercise?.let {
+            FavouritesAdapter(
+                list = it,
+                glide = Glide.with(this),
+                onItemClick = { exercise ->
+                    findNavController().navigate(
+                        R.id.action_favouritesFragment_to_infoFragment,
+                        InfoFragment.createBundle(exercise.id)
+                    )
+                }
+            )
+        }
+        binding?.rvFavourite?.adapter = adapter
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/impulse/WorkoutRepository.kt
+++ app/src/main/java/com/example/impulse/WorkoutRepository.kt
@@ -3,22 +3,22 @@ package com.example.impulse
 object WorkoutRepository {
     val list: List<Workout> = listOf(
         Workout(
-            title = "hands",
+            title = "Hands",
             desc = "100 best exercises",
             image = "https://i.ytimg.com/vi/5lToy2xj5rI/maxresdefault.jpg"
         ),
         Workout(
-            title = "press",
+            title = "Press",
             desc = "100 best exercises",
             image = "https://webpulse.imgsmail.ru/imgpreview?key=pulse_cabinet-image-81681b03-e83b-477e-9ad7-1d220385c0db&mb=webpulse"
         ),
         Workout(
-            title = "legs",
+            title = "Legs",
             desc = "100 best exercises",
             image = "https://sportishka.com/uploads/posts/2021-12/1639710217_77-sportishka-com-p-nakachennie-nogi-u-muzhchin-sport-krasvivo-80.jpg",
         ),
         Workout(
-            title = "back",
+            title = "Back",
             desc = "100 best exercises",
             image = "https://avatars.dzeninfra.ru/get-zen_doc/1567788/pub_5db9ea12c31e4900ad11d994_5db9ecc4ecfb8000ae7e1d9d/scale_1200"
         )
